d6ce8d00c2047f0e156deb6974b95bac5de81cec
Fitri <fitri@Fitris-MacBook-Air.local>
2018-03-08 13:45:59 +0000

    updated

--- ASV_Functions.py
+++ ASV_Functions.py
@@ -151,9 +151,9 @@ def Thruster_Values(LDM, Speed_PC): ## Speed_PC = percentage of max speed requir
             TiS.append(ESC_Fwd_Min + ((ESC_Fwd_Max - ESC_Fwd_Min)*Speed_PC) * Ti / max(abs(T)))
         if Ti < -T_cut_off:
             TiS.append(ESC_Rev_Min + ((ESC_Rev_Max - ESC_Rev_Min)*Speed_PC) * -Ti / max(abs(T)))
-        servo.setTarget(0, TiS)
-    	servo.setTarget(1, TiS)
-    	servo.setTarget(2, TiS)
+        servo.setTarget(0, TiS[0])
+    	servo.setTarget(1, TiS[1])
+    	servo.setTarget(2, TiS[2])
     #return(TiS)
 
 def turnLeft(Turn_Speed_PC):
