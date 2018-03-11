e281d941518ce722a0c23309c2bd59b097a04561
Fitri <fitri@Fitris-MacBook-Air.local>
2018-03-11 23:03:20 +0000

    TCP and UDP communication update

--- Communication/send.py
+++ Communication/send.py
@@ -2,13 +2,13 @@ import socket
 
 UDP_IP = '192.168.0.57'
 
-UDP_PORT = 5005
+UDP_PORT = 5545
 
 MESSAGE = "Hello, World!"
 
-print "UDP target IP:", UDP_IP
-print "UDP target port:", UDP_PORT
-print "message:", MESSAGE
+print("UDP target IP:", UDP_IP)
+print("UDP target port:", UDP_PORT)
+print("message:", MESSAGE)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
